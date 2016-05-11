33225c942c62f476d265d36f24c7268223de4990
Corey Bradley <coreytbradley@gmail.com>
2016-05-11 13:34:32 -0400

    Add oxford comma to string

--- mad.js
+++ mad.js
@@ -4,7 +4,7 @@ window.onload = function() {
         story = "<p>[Company] [Adverb] unveiled its latest device, and it's huge: meet the new [Device], rocking a whopping [Number 1]-inch display. \
 The company has been teasing its new [Device] for a while now, dropping images and videos to boost the hype before the big unveiling. We already knew about the [Adjective 4] screen, but let's see what else it brings to the table. </p>\
 <p>First off, [Company]'s Global Vice President [Full Name] claims that despite the huge display, the new [Company] [Device] is still \"extremely [Adjective 1].\" The screen is housed in a [Adjective 2] [Material] unibody, which makes the [Device] look stylish and premium.</p>\
-<p>Looks aside, other [Company] [Device] specifications include a [Noun 1] [Verb 1]er and the company's new [Adjective 3] camera, along with innovative software that allows the user to [Verb 2] their [Plural Noun] with friends. Color options include [Color 1], [Color 2] and [Noun 2] [Color 3].</p>\
+<p>Looks aside, other [Company] [Device] specifications include a [Noun 1] [Verb 1]er and the company's new [Adjective 3] camera, along with innovative software that allows the user to [Verb 2] their [Plural Noun] with friends. Color options include [Color 1], [Color 2], and [Noun 2] [Color 3].</p>\
 <p>As expected, the [Company] [Device] comes at a [Adjective 5] price, costing only $[Number 2] per month on-contract. \
 For more information on the [Company] [Device] specifications or to get a better look at the device's design, head over to [Company]'s website and take a look.</p>";
     var words = story.match(/([^[\]]+(?=]))(?!.*\1)/g);
