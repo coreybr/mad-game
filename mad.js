c0b99a26b96b135aa00b53c4844e17eb4cca990a
Corey Bradley <coreytbradley@gmail.com>
2016-05-13 09:44:07 -0400

    Add Job Opening story

--- mad.js
+++ mad.js
@@ -2,7 +2,8 @@ window.onload = function() {
     formContainer = document.getElementById("container--form");
     var selectStory = "<h3>Please select a story</h3> \
                         <ul>\
-                        <li><a href=\"javascript:loadForm(new_device);\">New Device</a></li>\
+                        <li><a href=\"javascript:loadForm(new_device);\" class =\"hvr-grow\">New Device Unveiling</a></li>\
+                        <li><a href=\"javascript:loadForm(job_opening);\" class =\"hvr-grow\">Job Opening</a></li>\
                         </ul>"  
     formContainer.innerHTML = selectStory;
     
@@ -14,6 +15,10 @@ The company has been teasing its new [Device 1] for a while now, dropping images
 <p>Looks aside, other [Company] [Device 1] specifications include a [Verb -ing] [Noun 1] and the company's new [Adjective 3] [Device 2], along with innovative software that allows the user to [Verb] their [Plural Noun] with friends. Color options include [Color 1], [Color 2], and [Noun 2] [Color 3].</p>\
 <p>As expected, the [Company] [Device 1] comes at a [Adjective 5] price, costing $[Number 2] per month on-contract. \
 For more information on the [Company] [Device 1] specifications or to get a better look at the device's design, head over to [Company]'s website and take a look.</p>";
+    
+    job_opening = "<p>[Location 1] residents who are passionate about [Verb -ing 1], autonomous [Machine]s, and making money are looking at a job opportunity from [Company].</p>\
+<p>The company is recruiting \"[Machine] safety specialists\" who have to watch the self-[Verb -ing 1] [Machine]s, ready to [Verb 1] should anything go south. The move comes as [Company] adds more [Machine]s to its fleet [Verb -ing 2] the [Plural Noun] of [Location 1]. The company offers $[Number] per hour to the brave souls who will venture in the monitoring of the autonomous [Machine]s. New hires will work under 12 to 24 [Unit of time] contracts that come with a non-disclosure agreement.</p>\
+<p>At the beginning of April 2016, [Company] proudly announced that its autonomous  [Machine] program expanded to [Location 1]. The company is eager to discover how the fleet will function in [Adjective 1] conditions. [Company] has already tested its autonomous [Machine]s in [Location 2], where it saw how they fared in [Adjective 2] weather.</p>";     
 };
 
 function loadForm(a_story) {
